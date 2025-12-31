7aa2b24affc6ea6cafebfcce8e4123b44530ab54
Ayush <ayushbijalwan.ab@gmail.com>
2025-12-31 18:00:00 +0530

    Checking test files

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,8 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define print(a) cout << a << "\n"
+#define printab(a,b) cout << a << " " << b << "\n"
+
 int main()
 {  
     cout << "Hello, World!" << endl;
     cout << "This project uses the C++17 language standard version." << endl;
+    int a;
+    cin >> a;
+    printab("You entered:", a);
     return 0;
 }
